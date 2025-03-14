794d1ed96fe5ed90fd9021a292f25384bdc17748
EduardoPulcino <eduardo.pulcino@sptech.school>
2025-03-14 00:14:15 -0300

    components e diretivas

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,82 @@
 <template>
+  <TheHeader
+    v-show="showHeader"
+  />
+
+  <div v-show="showName">
+    Nome: {{ name }} <br>
+    Sobrenome: {{ lastname }}
+  </div>
+
+  <div>
+    <div
+      v-for="(obj, index) in todos"
+      v-bind:key="obj.id"
+    >
+      {{ index + 1 }}  {{ obj.title }}
+    </div>
+  </div>
+
+  <div v-if="accessLevel === 'admin'">Admin</div>
+  <div v-else-if="accessLevel === 'marketing'">Marketing</div>
+  <div v-else>Normal</div>
+
+
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TheHeader
+  },
+
+  data() {
+    return {
+      todos: [
+      {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        }
+      ],
+      showHeader: true,
+      name: 'Jon',
+      lastname: 'Snow',
+      showName: false,
+      accessLevel: 'admin'
+    }
   }
 }
 </script>
